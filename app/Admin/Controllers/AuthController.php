3fa810e6895432a58c6d0c80b6dcdf4913f87190
yuf <335654255@qq.com>
2024-07-12 19:06:23 +0800

    channel

--- app/Admin/Controllers/AuthController.php
+++ app/Admin/Controllers/AuthController.php
@@ -2,9 +2,24 @@
 
 namespace App\Admin\Controllers;
 
+use Encore\Admin\Facades\Admin;
 use Encore\Admin\Controllers\AuthController as BaseAuthController;
 
 class AuthController extends BaseAuthController
 {
     protected $redirectTo = '/admin';
+
+    // 定义登录后的跳转路径
+
+    /**
+     * @return string
+     */
+    protected function redirectPath(): string
+    {
+        if(Admin::user()->isRole('channel')){
+            return admin_base_path('/car-channel/list');
+        }
+
+        return admin_base_path('/order/list');
+    }
 }
